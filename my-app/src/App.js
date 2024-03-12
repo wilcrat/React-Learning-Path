ea030962e4f797546cdcbf6454173393ff3d496e
Wilsonmobisa <willexjumah12338@gmail.com>
2024-03-12 23:48:36 +0300

    components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+function Header() {
+  return <h1>Hello Wilson</h1>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      This is the starting code for my first component
+      <Header />
     </div>
   );
 }
